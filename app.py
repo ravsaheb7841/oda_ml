8d3ed619b2257e783b3e0ebdc35ca7f317cc400d
ravsaheb7841 <bansoderav@gmail.com>
2024-09-11 23:14:00 +0530

    change

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ region_df = pd.read_csv("noc_regions.csv")
 df = preprocessor.preprocess(df, region_df)
 
 st.sidebar.title("Olympics Analysis")
-st.sidebar.image("C:/Users/banso/Data Analyst Project/Olympics/olympic.png")
 user_menu = st.sidebar.radio(
     "Select an option",
     ("Medal Tally", "Overall Analysis", "Country-wise Analysis", "Athlete wise Analysis")
